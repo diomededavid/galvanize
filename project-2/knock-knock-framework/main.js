990e72edf67bded5ab6dc7c98b4a3ce0df4c0997
David DIomede <david@d2creativedesign.com>
2016-11-23 15:07:01 -0700

    added knock knock project

---
+++ project-2/knock-knock-framework/main.js
@@ -0,0 +1,11 @@
+// Knock Knock JS Code: 
+
+document.getElementById("joke-part2").classList.add("hide");
+document.getElementById("button2").classList.add("hide");
+document.getElementById("joke-part-3").classList.add("hide");
+
+document.getElementById("button1").addEventListener("click", function(){
+	document.getElementById("joke-part2").classList.add("show");
+	
+	
+});
